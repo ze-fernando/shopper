39b533a40688fc8b223c26f7715a15066f3ed983
ze-fernando <jfengineer935@gmail.com>
2024-08-28 22:58:57 -0300

    fix: base64 bug

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import { uploadService } from './services/upload';
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.post('/upload', uploadService);
 
 app.listen(5000, () =>{
--- src/services/upload.ts
+++ src/services/upload.ts
@@ -4,7 +4,15 @@ import * as fs from 'fs';
 import isDoubleReport from "../utlis/validBody";
 
 export async function uploadService(req: Request, res: Response){
-    const {image, costumer_code, measure_datetime, measure_type} = req.body
+
+    if (!req.body) {
+        return res.status(400).json({
+            error_code: "INVALID_DATA",
+            error_description: "O corpo da requisição está vazio"
+        });
+    }
+
+    const { image, costumer_code, measure_datetime, measure_type } = req.body;
 
     if(!image || !costumer_code ||
         !measure_datetime || !measure_type){
@@ -20,9 +28,13 @@ export async function uploadService(req: Request, res: Response){
                 error_description: "Leitura do mês já realizada"
             });
         }
-        const imageBuffer = Buffer.from(image, 'base64');
-        const imagePath = `uploads/${Date.now()}temp.jpg`; 
-        fs.writeFileSync(imagePath, imageBuffer);
 
+        const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+
+        const imageBuffer = Buffer.from(base64Data, 'base64');
+        const imagePath = `uploads/${Date.now()}temp.jpg`; 
+        await fs.writeFileSync(imagePath, imageBuffer);
+
+        return res.status(200).json({message: 'tudo joiaa'});
 }
 
